a3837e0ca05c924126b64fbbe06531e19b9085eb
Vasilev Gennady <whidrubeld@gmail.com>
2021-03-16 00:46:56 +0300

    adding reminder for the future

---
+++ config-overrides.js
@@ -0,0 +1,5 @@
+module.exports = function override(config, env) {
+  // TODO: https://github.com/timarney/react-app-rewired
+  // adding babel-module-resolver & eslint-module-resolver
+  return config
+}
