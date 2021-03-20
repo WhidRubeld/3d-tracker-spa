6e880831ed196cce6d7e0e5dbf73456708426341
Vasilev Gennady <whidrubeld@gmail.com>
2021-03-21 00:01:17 +0300

    init store

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+
 import Navigation from './navigation'
+import store from '../store'
 
 export default function App() {
-  return <Navigation />
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  )
 }
