7a6dd04f2975fd145efc5a4f8cfefe922661bf5f
Vasilev Gennady <whidrubeld@gmail.com>
2021-04-08 22:57:43 +0300

    integrate notifications provider

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { createRef } from 'react'
+import { SnackbarProvider } from 'notistack'
+import { Collapse, IconButton } from '@material-ui/core'
+import { Close as CloseIcon } from '@material-ui/icons'
 import { Provider } from 'react-redux'
 
 import store from '../store'
@@ -6,11 +9,27 @@ import WatchSocketProvider from '../providers/watchSocketProvider'
 import Navigation from './navigation'
 
 export default function App() {
+  const notistackRef = createRef()
+  const onSnackDismiss = (key) => () => {
+    notistackRef.current.closeSnackbar(key)
+  }
+
   return (
-    <Provider store={store}>
-      <WatchSocketProvider>
-        <Navigation />
-      </WatchSocketProvider>
-    </Provider>
+    <SnackbarProvider
+      maxSnack={3}
+      TransitionComponent={Collapse}
+      ref={notistackRef}
+      action={(key) => (
+        <IconButton color='inherit' onClick={onSnackDismiss(key)}>
+          <CloseIcon />
+        </IconButton>
+      )}
+    >
+      <Provider store={store}>
+        <WatchSocketProvider>
+          <Navigation />
+        </WatchSocketProvider>
+      </Provider>
+    </SnackbarProvider>
   )
 }
