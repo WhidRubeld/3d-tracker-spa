5cdd3ec886ffd82cd837a30bce80d67a84c117a9
Vasilev Gennady <whidrubeld@gmail.com>
2021-04-09 00:37:33 +0300

    upgrade api methods

--- src/services/api.js
+++ src/services/api.js
@@ -2,14 +2,23 @@ import axios from 'axios'
 
 const API_URL = process.env.REACT_APP_API_URL
 
-const RACE_LIST_URL = `${API_URL}/admin/races`
-const RACE_SHOW_URL = (id) => `${API_URL}/admin/races/${id}`
+const RACE_BASE_URL = `${API_URL}/admin/races`
+const RACE_MODEL_URL = (id) => `${API_URL}/admin/races/${id}`
 
 export class ApiService {
+  static createRace(form) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(RACE_BASE_URL, form)
+        .then((res) => resolve(res.data.data))
+        .catch(reject)
+    })
+  }
+
   static getRaceList(page) {
     return new Promise((resolve, reject) => {
       axios
-        .get(RACE_LIST_URL, { params: { page } })
+        .get(RACE_BASE_URL, { params: { page } })
         .then((res) => resolve(res.data))
         .catch(reject)
     })
@@ -18,7 +27,16 @@ export class ApiService {
   static getRace(id, params = {}) {
     return new Promise((resolve, reject) => {
       axios
-        .get(RACE_SHOW_URL(id), { params })
+        .get(RACE_MODEL_URL(id), { params })
+        .then((res) => resolve(res.data.data))
+        .catch(reject)
+    })
+  }
+
+  static updateRace(id, form) {
+    return new Promise((resolve, reject) => {
+      axios
+        .put(RACE_MODEL_URL(id), form)
         .then((res) => resolve(res.data.data))
         .catch(reject)
     })
