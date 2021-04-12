2284c16dd789995ba4b09df0b616bb5fd61306b5
Vasilev Gennady <whidrubeld@gmail.com>
2021-04-12 20:19:10 +0300

    hotfix

--- src/components/modals/ManageRacerModal.jsx
+++ src/components/modals/ManageRacerModal.jsx
@@ -15,6 +15,9 @@ import LoadingButton from '../LoadingButton'
 
 import { ApiService } from '../../services'
 
+import { ValidatorForm } from 'react-material-ui-form-validator'
+import TextFieldValidator from '../form-validators/TextFieldValidator'
+
 import { reset as resetList } from '../../store/list'
 import { manageTracker as manageDetailsTracker } from '../../store/details'
 import { load as loadHistory } from '../../store/history'
@@ -22,7 +25,7 @@ import { manageTracker as manageWatchTracker } from '../../store/watch'
 
 const defaultValues = {
   name: null,
-  color_hex: null
+  color_hex: 'cccccc'
 }
 
 export default function ManageRacerModal({ race, racer, open, onClose }) {
@@ -42,6 +45,7 @@ export default function ManageRacerModal({ race, racer, open, onClose }) {
       setIsOpen(true)
       if (racer) {
         const { data: tracker } = racer.tracker
+        console.log(tracker)
         setForm({
           name: racer.name,
           color_hex: tracker.color_hex
@@ -138,43 +142,48 @@ export default function ManageRacerModal({ race, racer, open, onClose }) {
       disableBackdropClick={loading}
       aria-labelledby='race-modal-title'
     >
-      <DialogTitle id='race-modal-title'>
-        {!racer ? 'Новый участник' : 'Редактирование участника'}
-      </DialogTitle>
-      <DialogContent>
-        <TextField
-          autoFocus={!racer}
-          margin='normal'
-          label='Название участника'
-          type='text'
-          value={form.name}
-          onChange={(event) =>
-            setForm((v) => ({ ...v, name: event.target.value }))
-          }
-          disabled={loading}
-          required
-          fullWidth
-        />
-        <ColorBox
-          defaultValue={racer ? `#${racer.tracker.data.color_hex}` : undefined}
-          value={color}
-          onChange={handleChangeColor}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color='primary' disabled={loading}>
-          Отмена
-        </Button>
-        <LoadingButton
-          type='submit'
-          variant='contained'
-          color='primary'
-          loading={loading}
-          onClick={onSubmit}
-        >
-          {!racer ? 'Добавить' : 'Обновить'}
-        </LoadingButton>
-      </DialogActions>
+      <ValidatorForm onSubmit={onSubmit}>
+        <DialogTitle id='race-modal-title'>
+          {!racer ? 'Новый участник' : 'Редактирование участника'}
+        </DialogTitle>
+        <DialogContent>
+          <TextFieldValidator
+            autoFocus={!racer}
+            margin='normal'
+            label='Название участника'
+            type='text'
+            value={form.name}
+            onChange={(event) =>
+              setForm((v) => ({ ...v, name: event.target.value }))
+            }
+            validators={['required']}
+            errorMessages={['Поле обязательно для заполнения']}
+            disabled={loading}
+            required
+            fullWidth
+          />
+          <div>
+            <ColorBox
+              defaultValue={`#${form.color_hex}`}
+              value={color}
+              onChange={handleChangeColor}
+            />
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color='primary' disabled={loading}>
+            Отмена
+          </Button>
+          <LoadingButton
+            type='submit'
+            variant='contained'
+            color='primary'
+            loading={loading}
+          >
+            {!racer ? 'Добавить' : 'Обновить'}
+          </LoadingButton>
+        </DialogActions>
+      </ValidatorForm>
     </Dialog>
   )
 }
